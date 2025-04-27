a0d8705c35c7609d86f83aebbc3e802182c5e062
Letioska <letioska.dev@gmail.com>
2025-04-27 15:06:43 -0300

    Iniciando versionamento do meu projeto API Rest com Spring Boot e Maven

---
+++ meu-projeto-api-rest/src/main/java/learning/dio/meu_projeto_api_rest/MeuProjetoApiRestApplication.java
@@ -0,0 +1,13 @@
+package learning.dio.meu_projeto_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoApiRestApplication.class, args);
+	}
+
+}
---
+++ meu-projeto-api-rest/src/test/java/learning/dio/meu_projeto_api_rest/MeuProjetoApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.dio.meu_projeto_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
