76df3a5382d63aece5861119e68f6b4a4b371ba5
Letioska <letioska.dev@gmail.com>
2025-04-28 19:15:29 -0300

    Getters e setters das classes model

--- meu-projeto-api-rest/src/main/java/learning/dio/meu_projeto_api_rest/domain/model/User.java
+++ meu-projeto-api-rest/src/main/java/learning/dio/meu_projeto_api_rest/domain/model/User.java
@@ -3,11 +3,58 @@ package learning.dio.meu_projeto_api_rest.domain.model;
 import java.util.List;
 
 public class User {
+    private Long id;
     private String name;
     private Account account;
     private List<Feature> features;
     private Card card;
     private List<News> news;
 
-    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
 }
